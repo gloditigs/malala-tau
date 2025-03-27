dfcf0f3a96ed9620578dddd799c80ed0caa53156
gloditigs <bennymbala04@gmail.com>
2025-03-27 14:27:24 +0200

    db

--- config/db.js
+++ config/db.js
@@ -2,15 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    console.log('Attempting to connect to:', process.env.MONGODB_URI.replace(/:([^@]+)@/, ':****@')); // Mask password
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000 // 30 seconds timeout
-    });
+    await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('MongoDB connected');
   } catch (err) {
-    console.error('Connection error:', err.message);
+    console.error(err.message);
     process.exit(1);
   }
 };
