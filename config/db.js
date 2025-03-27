bb041e4cc1989dbd8f38cc8bbe9acfad736ce818
gloditigs <bennymbala04@gmail.com>
2025-03-27 15:42:22 +0200

    MONGODB

--- config/db.js
+++ config/db.js
@@ -1,19 +1,32 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
+let cachedConnection = null;
+
 const connectDB = async () => {
+  if (cachedConnection) {
+    console.log('Using cached MongoDB connection');
+    return cachedConnection;
+  }
   try {
     console.time('MongoDB Connection');
     console.log('Connecting to:', process.env.MONGODB_URI);
-    await mongoose.connect(process.env.MONGODB_URI, {
-      serverSelectionTimeoutMS: 9000 // 9 seconds
+    const connection = await mongoose.connect(process.env.MONGODB_URI, {
+      serverSelectionTimeoutMS: 9500, // 9.5 seconds
+      connectTimeoutMS: 9500,        // Max connection time
+      socketTimeoutMS: 9500          // Max socket wait
     });
     console.timeEnd('MongoDB Connection');
     console.log('MongoDB connected');
+    cachedConnection = connection;
+    return connection;
   } catch (err) {
     console.error('Connection error:', err.message);
     process.exit(1);
   }
 };
+
+// Pre-connect on startup
+connectDB().catch(err => console.error('Pre-connection failed:', err.message));
 
 module.exports = connectDB;
